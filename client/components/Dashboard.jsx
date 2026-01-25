15e503bbaade4fdc2cf17df7dbaa3b8b04f8259b
RRARI504 <ryansimien@ymail.com>
2026-01-25 14:24:17 -0600

    "FIX: Added JSDoc-style state description, fixed update, need to add FE for update"

--- client/components/Dashboard.jsx
+++ client/components/Dashboard.jsx
@@ -7,21 +7,46 @@ import { Box, Button , Dialog, DialogContent, TextField, FormControl, InputLabel
 import JobList from "./JobList.jsx"
 
 
-//need to import statuses or set them here
+//Need to import statuses or set them here
 const jobStatus = ["applied", "interviewing", "offer", "rejected"]
 
-//create dashboard component
 export default function Dashboard ({ currentUser }) {
+  /**
+   * A state variable that holds the list of job objects.
+   * Updating state triggers re-render for job-list when job state changes.
+   * @type {array}
+   * @name jobs
+  */
+  const [jobs, setJobs] = useState([]);
 
-  //state of jobs
-  const [jobs, setJobs] = useState([]);//jobs need re render when jobs state changes
-  // visibility of create jobs dialog false = hidden
+  /**
+   * A state variable that controls visibility of 'Create Job' dialog.
+   * (False = hidden || True = visible)
+   * @type {boolean}
+   * @name openDialog
+  */
   const [openDialog, setDialog] = useState(false);
-  //state of title, adding a new job title
+
+  /**
+   * A state variable that stores title of new job being created.
+   * @type {string}
+   * @name title
+ */
   const [title, setTitle ] = useState("");
-  //state of status, adding a status to job
-  const [status, setStatus] = useState("applied"); //default to 'applied'
-  //state of job link
+
+  /**
+   * A state variable that stores title of new job being created.
+   * //default to 'applied'
+   * @type {string}
+   * @name status
+ */
+  const [status, setStatus] = useState("applied");
+
+  /**
+   * A state variable that stores state of job link associated with job posting.
+   * @type {string}
+   * @name link
+ */
   const [link, setLink] = useState("");
 
   // REPORTING STUFF
@@ -114,22 +139,23 @@ export default function Dashboard ({ currentUser }) {
   }
 
   //handle updating a job
-   //const updateJob = (jobId, newStatus) => {
-  //   //put to backend to update a job w/ by id
-  //   axios.put(`/api/jobs/${jobId}`, {
-  //     //set current state of status to the new status
-  //     status: newStatus
-  //   }).then((updated) => {
-  //      //get updated jobs data in response
-  //     const updatedJob = updated.data;
+   const updateJob = (jobId, newStatus) => {
+    //put to backend to update a job w/ by id
+    axios.put(`/api/jobs/${jobId}`, {
+      //set current state of status to the new status
+      status: newStatus
+    }).then((updated) => {
+       //get updated jobs data in response
+      const updatedJob = updated.data;
 
+      setJobs(prevJobs => prevJobs.map(job =>
+        job._id === updatedJob._id ? updatedJob : job
+    ))
+    }).catch((err) => {
+      console.log(err);
+    });
 
-  //     ))
-  //   }).catch((err) => {
-  //     console.log(err);
-  //   });
-   //}
-
+  }
 
     //handle deleting a job
    const deleteJob = (jobId) => {
@@ -185,7 +211,7 @@ export default function Dashboard ({ currentUser }) {
           <Button onClick={CreateJob} variant="contained" color="primary"> Save </Button>
         </DialogActions>
         </Dialog>
-        <JobList jobs={jobs} /*onUpdate={updateJob}*/ onDelete={deleteJob} currentUser={currentUser} />
+        <JobList jobs={jobs} onUpdate={updateJob} onDelete={deleteJob} />
 
     </Box>
 
